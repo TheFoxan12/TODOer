0596f0276e5fd7bea32d9777d33114d5d0aad3f2
thibautunsinger <thibaut.unsinger@gmail.com>
2024-09-29 13:47:56 +0200

    Feat: added new custon main color

--- app/src/main/java/fr/quarantedeuxmulhouse/tunsinge/todoer/ui/theme/Color.kt
+++ app/src/main/java/fr/quarantedeuxmulhouse/tunsinge/todoer/ui/theme/Color.kt
@@ -1,6 +1,8 @@
 package fr.quarantedeuxmulhouse.tunsinge.todoer.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import fr.quarantedeuxmulhouse.tunsinge.todoer.R
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
@@ -9,3 +11,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val LightTodoYellow = Color(0xFFFFEB3B)
+val DarkTodoYellow = Color(0xFF73641A)
