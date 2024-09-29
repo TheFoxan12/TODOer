6450101384c13e6454e2c905c2ce0cfde26427b5
thibautunsinger <thibaut.unsinger@gmail.com>
2024-09-29 15:16:57 +0200

    Feat: changed main page, added top bar

--- app/src/main/java/fr/quarantedeuxmulhouse/tunsinge/todoer/MainActivity.kt
+++ app/src/main/java/fr/quarantedeuxmulhouse/tunsinge/todoer/MainActivity.kt
@@ -5,17 +5,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import fr.quarantedeuxmulhouse.tunsinge.todoer.task.TaskScreen
@@ -28,6 +38,7 @@ class MainActivity : ComponentActivity() {
         TaskListViewModelFactory((application as TodoerApplication).repository)
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen().apply {
@@ -38,17 +49,44 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TODOerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { padding ->
-                    Column() {
-                        Text(
-                            modifier = Modifier
-                                .padding(padding)
-                                .align(Alignment.CenterHorizontally),
-                            text = "TODOer",
-                            style = TextStyle(fontSize = 40.sp)
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            colors = topAppBarColors(
+                                containerColor = colorScheme.background,
+                                titleContentColor = colorScheme.primary,
+                            ),
+                            title = {
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    Image(
+                                        painter = painterResource(id = R.drawable.todo_icon),
+                                        contentDescription = "todo_icon",
+                                        Modifier.size(
+                                            width = 100.dp,
+                                            height = 100.dp
+                                        )
+                                    )
+                                    Text(
+                                        "TODOer",
+                                        style = TextStyle(fontSize = 40.sp)
+                                    )
+                                }
+                            }
                         )
-                        TaskScreen(taskManager = taskManager,
-                            modifier = Modifier.windowInsetsPadding(WindowInsets.safeDrawing))
+                    },
+                    containerColor = colorScheme.background
+                ) { padding ->
+                    Column(
+                        modifier = Modifier.padding(padding)
+                    ) {
+                        TaskScreen(
+                            modifier = Modifier.windowInsetsPadding(WindowInsets.safeDrawing),
+                            taskManager = taskManager,
+                        )
                     }
                 }
             }
