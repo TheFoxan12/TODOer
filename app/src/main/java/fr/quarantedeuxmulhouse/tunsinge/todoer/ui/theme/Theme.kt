3cd2b96ad43e7ad474d7c9e109bf316927d02e38
thibautunsinger <thibaut.unsinger@gmail.com>
2024-09-29 15:17:45 +0200

    Fix: slightly changed colors

--- app/src/main/java/fr/quarantedeuxmulhouse/tunsinge/todoer/ui/theme/Theme.kt
+++ app/src/main/java/fr/quarantedeuxmulhouse/tunsinge/todoer/ui/theme/Theme.kt
@@ -13,14 +13,14 @@ private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
     tertiary = Pink80,
-    background = DarkTodoYellow
+    background = Yellow900
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40,
-    background = LightTodoYellow
+    background = Yellow,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
